943ad7cbf14e25608cd9a86f359e145cc3cbda9c
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-01-13 03:27:43 +0600

    added the origin

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 9000
 const app = express()
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'https://consep-session2.web.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
